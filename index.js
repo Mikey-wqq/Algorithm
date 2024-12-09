99835a3ba14ca0fa46785ad35f838fd7267adc15
Mikey-wqq <sidneymichael@gmail.com>
2024-12-09 12:02:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Adding Three Numbers 
+var num1 = 17
+var num2 = 25
+var num3 = 12
+
+var sum = num1 + num2 + num3 
+
+console.log(sum)
+
+// Selection proceessing
+var age = 25
+if(age>17){
+    console.log("You can vote")
+} else{
+    console.log("You can't vote")
+}
