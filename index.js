4fee1f89b1e17434b01acf751ba34fe02220cae6
Mikey-wqq <sidneymichael@gmail.com>
2024-12-11 09:43:01 +0100

    first commit

--- index.js
+++ index.js
@@ -14,3 +14,108 @@ if(age>17){
 } else{
     console.log("You can't vote")
 }
+
+// variable declaration
+var fullName
+var Married
+var numberOfChildren
+var disability
+var accountNumber
+
+// variable assignment
+fullName = "Michael Sidney"
+Married = false
+numberOfChildren = 0
+disablility = null
+accountNumber = undefined
+
+// variable initiallization
+var fullName = 'Michael Sidney'
+var Married = false 
+var numberOfChildren = 0
+var disability = null 
+var accountNumber = undefined 
+
+// Console.log
+console.log(fullName);
+console.log(numberOfChildren);
+console.log(20+20);
+console.log('hello world');
+
+
+// String operators
+var sentence = 'I am going to be a badass software developer'
+console.log('sentence.length');
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+
+// Concatenation
+var favouriteTeam = 'tottenham' 
+var favouritePlayer = 'son'
+var ability =  'world class'
+var sentence2 = 'my favourite player is ' + favouritePlayer + ' he plays for ' + favouriteTeam + ' he is ' + ability 
+console.log(sentence2);
+
+// arithmetic operators
+var x = 5
+var y = 3
+var z = 25
+console.log(z-x);
+console.log(x+y);
+console.log(y*z);
+console.log(z%x);
+console.log(z**z);
+console.log(x/y);
+
+// math operators
+console.log(Math.sqrt(144));
+console.log(Math.pow(16,8));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.6));
+console.log(Math.ceil(4.4));
+
+// Assignment operators
+var num1 = 25
+var num2 = 35 
+var num3 = 15
+
+num1 += num3
+console.log(num1);
+num2 -= num3
+console.log(num2);
+num3 -= num2 
+console.log(num3);
+
+// Comparison operators
+console.log(5==5);
+console.log(5==6);
+console.log(5==='5');
+console.log(5!=6);
+console.log(5!=='5');
+console.log(100>17);
+console.log(18>=18);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
